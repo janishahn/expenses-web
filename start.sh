083783f1f0a7a7a5b21ad002177cb18bb0330764
janishahn <hahn.janis@gmail.com>
2025-12-15 00:00:40 +0100

    Change start.sh to not use uv

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PROJECT_DIR="~/Documents/expenses-web"
 cd "$PROJECT_DIR"
-uv run python -m uvicorn main:app --reload --port 8001 --host 0.0.0.0
+source .venv/bin/activate
+uvicorn main:app --reload --port 8001 --host 0.0.0.0
 
